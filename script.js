07de998f92e0b35bed127ef645492d9affac001a
mhmmdhanafi <134913672+mhmmdhnfi@users.noreply.github.com>
2026-06-13 18:22:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const text = [
+    "Instrumentation Engineer",
+    "Control System Engineer",
+    "Automation Engineer",
+    "IoT Developer"
+];
 
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+    if (count === text.length) {
+        count = 0;
+    }
+
+    currentText = text[count];
+
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if (letter.length === currentText.length) {
+
+        count++;
+
+        index = 0;
+
+        setTimeout(type, 1500);
+
+    } else {
+
+        setTimeout(type, 100);
+
+    }
+
+})();
